af04a7d712bbf605fc6e3d76dcd199f61ad93a12
longhaogao <1207240302@qq.com>
2019-05-14 22:47:22 +0800

    hello world

---
+++ QuizCard.java
@@ -0,0 +1,17 @@
+package QuizCardPlayer;
+
+public class QuizCard {
+    private String question;
+    private String answer;
+
+    QuizCard(String q,String a){
+        q=question;
+        a=answer;
+    }
+    public String getQuestion(){
+        return question;
+    }
+    public String getAnswer(){
+        return answer;
+    }
+}
