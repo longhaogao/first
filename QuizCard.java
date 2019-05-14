4cd0f123308d1c28932258edbde750ec7593888e
longhaogao <49892429+longhaogao@users.noreply.github.com>
2019-05-14 22:55:51 +0800

    新增变量
    新增一个变量test

--- QuizCard.java
+++ QuizCard.java
@@ -3,10 +3,14 @@ package QuizCardPlayer;
 public class QuizCard {
     private String question;
     private String answer;
+    private String test;
 
     QuizCard(String q,String a){
         q=question;
         a=answer;
+    }
+    public void setTest(String test){
+        this.test=test;
     }
     public String getQuestion(){
         return question;
